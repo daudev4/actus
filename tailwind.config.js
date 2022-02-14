ee668b2a4aea4e6d90d34a551c90ab84badd3c33
Daud Sultygov <d.sultygov@fbs.com>
2022-02-14 22:42:39 +0300

    header component creation

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,23 @@ module.exports = {
       './src/**/*.css',
   ],
   theme: {
-    extend: {},
+    fontSize: {
+      base: ['14px', '16px'],
+    },
+    borderRadius: {
+      DEFAULT: '5px',
+    },
+    extend: {
+      colors: {
+        'gray-100': '#f8f8f8',
+        'gray-200': '#E5E5E5',
+        'gray-300': '#D6D6D6',
+        'gray-400': '#787878',
+      },
+      gridTemplateColumns: {
+        'header': 'minmax(auto, 1fr) auto minmax(auto, 1fr)'
+      },
+    },
   },
   plugins: [],
 }
